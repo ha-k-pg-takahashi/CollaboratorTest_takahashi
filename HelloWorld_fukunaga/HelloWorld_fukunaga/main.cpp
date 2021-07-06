b39a3cbbb116bb4e2dd469846c408f5537dfb4ee
MSI\dqxre <dqx.reef@gmail.com>
2021-07-06 16:39:55 +0900

    福永フォルダ追記自己名前

--- HelloWorld_fukunaga/HelloWorld_fukunaga/main.cpp
+++ HelloWorld_fukunaga/HelloWorld_fukunaga/main.cpp
@@ -1,11 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include"takashi2.h"
 
 int main(void)
 {
 	printf("Hello World");
-	
+	Takashi2 myTakashi;
+	myTakashi.ShowName();
+
 	system("pause");
 	return 0;
 }
