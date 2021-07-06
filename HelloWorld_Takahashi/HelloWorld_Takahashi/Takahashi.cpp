68b55e59a6dd269b528f1c0e05efbe96eae7ffce
MSI\dqxre <dqx.reef@gmail.com>
2021-07-06 16:12:47 +0900

    名前出力

--- HelloWorld_Takahashi/HelloWorld_Takahashi/Takahashi.cpp
+++ HelloWorld_Takahashi/HelloWorld_Takahashi/Takahashi.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include"takahashi.h"
 
-void ShowName()
+void Takahashi::ShowName()
 {
 	printf("ûü‹´\n");
 }
--- HelloWorld_Takahashi/HelloWorld_Takahashi/main.cpp
+++ HelloWorld_Takahashi/HelloWorld_Takahashi/main.cpp
@@ -1,7 +1,11 @@
 #include<stdio.h>
+#include"takahashi.h"
 
 int main()
 {
 	printf("HelloWorld\n");
+	Takahashi my;
+	my.ShowName();
+
 	return 0;
 }
